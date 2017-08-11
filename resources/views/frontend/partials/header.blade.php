876c066e3ade5cfb2786391efd541ab1494c837b
HoangNH <hoangnhonline@gmail.com>
2017-08-11 11:59:11 +0700

    .

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -73,8 +73,8 @@
 							Máy cũ giá rẻ
 						</a>
 					</li><!-- level0 -->
-					@foreach($loaiSpList as $loaiSp)
-					<li class="level0 parent {{ isset($loaiDetail) && $loaiDetail->id == $loaiSp->id ? "active" : "" }}">
+					@foreach($loaiSpList as $loaiSp)					
+					<li class="level0 parent {{ $routeName != 'search' && isset($loaiDetail) && $loaiDetail->id == $loaiSp->id ? "active" : "" }}">
 						<a href="{{ route('parent-cate', $loaiSp->slug) }}" title="{!! $loaiSp->name !!}">
 							<i class="fa fa-mobile"> </i>
 							{!! $loaiSp->name !!}
